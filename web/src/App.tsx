a05adad990c6d67ae9679a541030486aa0ab1f1d
valdir-ti <valdir.ti@gmail.com>
2020-10-10 17:31:18 -0300

    End styles mobile first landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
+import Landing from './pages/Landing';
+
 import './assets/styles/globals.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>NLW 2</h2>
-    </div>
+    <Landing />   
   );
 }
 
