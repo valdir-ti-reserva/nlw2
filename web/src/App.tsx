2177d9db1ecee26ac7a44c1e83bb2ae3ce12c57d
valdir-ti <valdir.ti@gmail.com>
2020-10-11 13:47:02 -0300

    Create navigation with react-router-dom

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-
-import Landing from './pages/Landing';
+import Routes from './routes';
 
 import './assets/styles/globals.css';
 
 function App() {
   return (
-    <Landing />   
+    <Routes />
   );
 }
 
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeacherList (){
+  return (
+    <h2>Teacher List</h2>
+  )
+}
+
+export default TeacherList;
