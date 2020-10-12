ec653d2a10d8463509865079635e0505865a2b8d
valdir-ti <valdir.ti@gmail.com>
2020-10-12 00:59:21 -0300

    Adapted to use component Input

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Input from '../../components/Input';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
+
 
 import './styles.css';
 
@@ -10,18 +12,10 @@ function TeacherList (){
     <div id="page-teacher-list" className="container">
       <PageHeader title="Esses são os proffys disponíveis">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>            
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da Semana</label>
-            <input type="text" id="week_day"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          
+          <Input label="Matéria" name="subject" />          
+          <Input label="Dia da Semana" name="week_day" />          
+          <Input label="Hora" name="time" type="time" />
 
         </form>
       </PageHeader>
