3c622de5cc83eb3b5f47c4fc53db73f8c41cf292
valdir-ti <valdir.ti@gmail.com>
2020-10-12 01:32:42 -0300

    Adapting to use news Components

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Input from '../../components/Input';
 
 import PageHeader from '../../components/PageHeader';
+import Select from '../../components/Select';
 import TeacherItem from '../../components/TeacherItem';
 
 
@@ -13,8 +14,33 @@ function TeacherList (){
       <PageHeader title="Esses são os proffys disponíveis">
         <form id="search-teachers">
           
-          <Input label="Matéria" name="subject" />          
-          <Input label="Dia da Semana" name="week_day" />          
+          <Select 
+            label="Matéria" 
+            name="subject" 
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Português', label: 'Português' },
+            ]}
+          />
+          
+          <Select 
+            label="Dia da Semana" 
+            name="week_day" 
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' },
+            ]}
+          />
+          
           <Input label="Hora" name="time" type="time" />
 
         </form>
