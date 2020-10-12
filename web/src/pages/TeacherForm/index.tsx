db8464d205b9b9cd40b1b19c0d237b8fbce90089
valdir-ti <valdir.ti@gmail.com>
2020-10-12 01:00:15 -0300

    Styles Teacher form from mobile and desktop

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,11 +1,51 @@
 import React from 'react';
+
+import Input from '../../components/Input';
 import PageHeader from '../../components/PageHeader';
 
+import warningIcon from '../../assets/images/icons/warning.svg'
+
+import './styles.css';
 
 function TeacherForm (){
   return (
-    <div id="page-teacher-list" className="container">
-      <PageHeader title="Que incrível que você quer dar aulas" />
+    <div id="page-teacher-form" className="container">
+      <PageHeader 
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+
+        <fieldset>
+          <legend>Seus Dados</legend>
+          <Input label="Nome Completo" name="name" autoComplete="off" />
+          <Input label="Avatar" name="avatar" autoComplete="off"/>
+          <Input label="WhatsApp" name="whatsapp" autoComplete="off"/>          
+        </fieldset>
+        
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input label="Matéria" name="subject" autoComplete="off" />
+          <Input label="Custo da sua hora por aula (em R$)" name="cost" autoComplete="off"/>
+        </fieldset>
+        
+        <fieldset>
+          <legend>Horários disponíveis</legend>
+          <Input label="Nome Completo" name="name" autoComplete="off" />
+          <Input label="Avatar" name="avatar" autoComplete="off"/>
+          <Input label="WhatsApp" name="whatsapp" autoComplete="off"/>          
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Warning Icon"/>
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type="button">Salvar Cadastro</button>
+        </footer>
+      </main>
     </div>
   )
 }
